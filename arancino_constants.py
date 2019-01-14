71fbdf9ffd9c891035757b457a697b136eb3c9dd
sergio <sergio@smartme.io>
2019-01-14 23:53:00 +0100

    add constants file, arancino port discovery

---
+++ arancino_constants.py
@@ -0,0 +1,48 @@
+'''
+
+Copyright ® SmartMe.IO  2018
+
+LICENSE HERE
+
+Filename = arancino_properties.py
+Author: Sergio Tomasello - sergio@smartme.io
+Date: 2019 01 14
+
+'''
+
+#Definitions for Serial Protocol
+CHR_EOT = chr(4)            #End Of Transmission Char
+CHR_SEP = chr(30)           #Separator Char
+
+
+CMD_SYS_START   = 'START' #Start Commmand
+
+CMD_APP_GET     = 'GET'     #Get value at key
+CMD_APP_SET     = 'SET'     #Set value at key
+CMD_APP_DEL     = 'DEL'     #Delete one or multiple keys
+CMD_APP_KEYS    = 'KEYS'    #Get keys by a pattern
+CMD_APP_HGET    = 'HGET'    #
+CMD_APP_HGETALL = 'HGETALL' #
+CMD_APP_HKEYS   = 'HKEYS'   #
+CMD_APP_HVALS   = 'HVALS'   #
+CMD_APP_HDEL    = 'HDEL'    #
+CMD_APP_HSET    = 'HSET'    #
+CMD_APP_PUB     = 'PUB'     #
+CMD_APP_FLUSH   = 'FLUSH'   #Flush the current Database, delete all the keys from the current Database
+
+RSP_OK          = '100'     #OK Response
+RSP_HSET_NEW    = '101'     #Set value into a new field
+RSP_HSET_UPD    = '102'     #Set value into an existing field
+
+ERR             = '200'     #Generic Error
+ERR_NULL        = '201'     #Null value
+ERR_SET         = '202'     #Error during SET
+ERR_CMD_NOT_FND = '203'     #Command Not Found
+ERR_CMD_NOT_RCV = '204'     #Command Not Received
+ERR_CMD_PRM_NUM = '205'     #Invalid parameter number
+ERR_REDIS       = '206'     #Generic Redis Error
+
+#Reserved keys
+RSVD_KEY_MONITOR = "___MONITOR___"
+RSVD_KEY_LIBVERSION = "___LIBVERS___"
+RSVD_KEY_MODVERSION = "___MODVERS___"
--- arancino_port.py
+++ arancino_port.py
@@ -4,45 +4,73 @@ Copyright ® SmartMe.IO  2018
 
 LICENSE HERE
 
+Filename = arancino_port.py
 Author: Sergio Tomasello - sergio@smartme.io
 Date: 2019 01 14
 
 '''
 
+from serial.tools import list_ports as list
+
+class ArancinoPortManager:
+
+    def __init__(self, devicestore):
+        self.__devicestore = devicestore
+
+    def getPluggedArancinoPorts(self):
+        __ports = list.comports()
+        __ports = self.__filterSerialPorts(__ports)
+        __ports = self.__transformInArancinoPorts(__ports)
+        return __ports
+
+    def __filterSerialPorts(self, ports):
+        """
+        Filters Serial Ports with valid Serial Number, VID and PID
+        :param ports: List of ListPortInfo
+        :return ports_filterd: List
+        """
+
+        ports_filterd = []
+
+        for port in ports:
+            if port.serial_number != None and port.serial_number != "FFFFFFFFFFFFFFFFFFFF" and port.vid != None and port.pid != None:
+                ports_filterd.append(port)
+
+        return ports_filterd
+
+    def __transformInArancinoPorts(self, ports):
+        """
+        This methods creates a new structure starting from a List of ListPortInfo.
+        A base element of the new structure is composed by some metadata and
+        the initial object of type ListPortInfo
 
 
+        :param ports: List of plugged port (ListPortInfo)
+        :return: Dict of Ports with additional information
+        """
+        new_ports_struct = {}
+
+        for port in ports:
+            p = ArancinoPort(plugged=True, port=port)
+            new_ports_struct[p.id] = p
+
+        return new_ports_struct
 
 
 class ArancinoPort:
 
-    # serial port identification
-    id = None
 
-    # configuration metadata
-    enabled = False
-    autoreconnect = False
-    alias = None
-
-    # status metadata
-    plugged = False
-    connected = False
-    datetime = None
-
-    # serial port
-    port = None
-
-
-    def __init__(self, enabled = False, autoreconnect = False, alias = None, plugged = False, connected = False, port = None ):
+    def __init__(self, enabled = False, autoconnection = False, alias = None, plugged = False, connected = False, port = None ):
 
         # serial port
         self.port = port
 
         # serial port identification
-        self.id = port.serial_number
+        #self.id = port.serial_number
 
         # configuration metadata
         self.enabled = enabled
-        self.autoreconnect = autoreconnect
+        self.autoconnection = autoconnection
         self.alias = alias
 
         # status metadata
@@ -51,4 +79,66 @@ class ArancinoPort:
 
         #todo datetime attribute
 
+    @property
+    def port(self):
+        return self.__port
 
+
+    @port.setter
+    def port(self, port):
+        self.__port = port
+        #self.__id = port.serial_number
+
+
+    @property
+    def id(self):
+        return self.__id
+
+
+    @property
+    def enabled(self):
+        return self.__enabled
+
+
+    @enabled.setter
+    def enabled(self, enabled):
+        self.__enabled = enabled
+
+    @property
+    def autoconnection(self):
+        return self.__autoreconnect
+
+
+    @autoconnection.setter
+    def autoconnection(self, autoconnection):
+        self.__autoconnection = autoconnection
+
+
+    @property
+    def alias(self):
+        return self.__alias
+
+
+    @alias.setter
+    def alias(self, alias):
+        self.__alias = alias
+
+
+    @property
+    def plugged(self):
+        return self.__plugged
+
+
+    @plugged.setter
+    def plugged(self, plugged):
+        self.__plugged = plugged
+
+
+    @property
+    def connected(self):
+        return self.__connected
+
+
+    @connected.setter
+    def connected(self, connected):
+        self.__connected = connected
