65f73b6cf0bc4b8324cf8bde04e61608584b522e
sergio <sergio@smartme.io>
2020-03-20 00:56:31 +0100

    trying to solve #50

--- extras/post-install.sh
+++ extras/post-install.sh
@@ -31,20 +31,26 @@ cp extras/arancino.service /etc/systemd/system/
 echo --------------------------------------
 
 echo --------------------------------------
-echo Checking diff of configuration file and making a backup
-#copy arancino config file to /etc/arancino/config <== ARANCINOCONF and make a backup of current conf file, if different
-crc_new=$(md5sum config/arancino.cfg | awk {'print $1'})
-crc_old=$(md5sum /etc/arancino/config/arancino.cfg | awk {'print $1'})
+#echo Checking diff of configuration file and making a backup
+##copy arancino config file to /etc/arancino/config <== ARANCINOCONF and make a backup of current conf file, if different
+#crc_new=$(md5sum config/arancino.cfg | awk {'print $1'})
+#crc_old=$(md5sum /etc/arancino/config/arancino.cfg | awk {'print $1'})
+#timestamp=$(date +%Y%m%d_%H%M%S)
+#
+#if [ "$crc_new" != "$crc_old" ]
+#then
+#    echo Creating configuration backup file "/etc/arancino/config/arancino_$timestamp.cfg"
+#    mv /etc/arancino/config/arancino.cfg /etc/arancino/config/arancino_$timestamp.cfg
+#    cp config/arancino.cfg /etc/arancino/config/arancino.cfg
+#fi
+
+echo Backup previous configurations files
 timestamp=$(date +%Y%m%d_%H%M%S)
-
-if [ "$crc_new" != "$crc_old" ]
-then
-    echo Creating configuration backup file "/etc/arancino/config/arancino_$timestamp.cfg"
-    mv /etc/arancino/config/arancino.cfg /etc/arancino/config/arancino_$timestamp.cfg
-    cp config/arancino.cfg /etc/arancino/config/arancino.cfg
-fi
-
+mv /etc/arancino/config/arancino.cfg /etc/arancino/config/arancino_$timestamp.cfg
+mv /etc/arancino/config/arancino.test.cfg /etc/arancino/config/arancino_$timestamp.test.cfg
+cp config/arancino.cfg /etc/arancino/config/arancino.cfg
 cp config/arancino.test.cfg /etc/arancino/config/arancino.test.cfg
+
 echo --------------------------------------
 
 echo --------------------------------------
