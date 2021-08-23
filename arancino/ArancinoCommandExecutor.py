b535ec259e1dd684ec81ca11639af457757769cc
Gingex <m.a.ingegneri@gmail.com>
2021-08-23 15:17:52 +0200

    fix: create ts key with retention

--- arancino/ArancinoCommandExecutor.py
+++ arancino/ArancinoCommandExecutor.py
@@ -1177,7 +1177,7 @@ class ArancinoCommandExecutor:
             if not self.__conf.get_serial_number() == "0000000000000000" and not self.__conf.get_serial_number() == "ERROR000000000":
                 labels["device_id"] = self.__conf.get_serial_number()
 
-            self.__datastore_tser.create(key, labels=labels, duplicate_policy='last', retation=self.__conf.get_redis_timeseries_retation())
+            self.__datastore_tser.create(key, labels=labels, duplicate_policy='last', retention_msecs=self.__conf.get_redis_timeseries_retation())
             self.__datastore_tser.redis.set("{}:{}".format(key, SUFFIX_TMSTP), 0)  # Starting timestamp
 
     #endregion
