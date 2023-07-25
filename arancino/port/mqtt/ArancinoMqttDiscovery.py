abaff3d55a9237f7cf0ea7577805586dacf9e4e6
sergio <sergio@smartme.io>
2023-07-25 09:40:23 +0200

    fix reset on connect mqtt port #113

--- arancino/port/mqtt/ArancinoMqttDiscovery.py
+++ arancino/port/mqtt/ArancinoMqttDiscovery.py
@@ -59,7 +59,7 @@ class ArancinoMqttDiscovery(object):
         self.__mqtt_arancino_daemon_ca_certs = CONF.get("port").get("mqtt").get("connection").get("ca_path")
         self.__mqtt_arancino_daemon_certfile = CONF.get("port").get("mqtt").get("connection").get("cert_path")
         self.__mqtt_arancino_daemon_keyfile = CONF.get("port").get("mqtt").get("connection").get("key_path")
-        self.__mqtt_arancino_reset_on_connect = CONF.get("port").get("mqtt").get("connection").get("reset_on_connect")
+        self.__mqtt_arancino_reset_on_connect = CONF.get("port").get("mqtt").get("reset_on_connect")
 
         
         try:
